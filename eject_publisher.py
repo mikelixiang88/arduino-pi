cf863a9e6d236eae1d77fefc577f02c83b3decfb
Jade Minkoff <minkoff@usc.edu>
2022-06-25 23:59:19 -0700

    Add files via upload

---
+++ eject_publisher.py
@@ -0,0 +1,41 @@
+
+
+"Run eject_publisher.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+import time
+from pynput import keyboard
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+    #subscribe to topics of interest here
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+def on_press(key):
+    try:
+        k = key.char # single-char keys
+    except:
+        k = key.name # other keys
+
+    if k == 'w':
+        #send "w" character to rpi
+        client.publish("trojancharge/servo", "eject")
+    
+if __name__ == '__main__':
+    #setup the keyboard event listener
+    lis = keyboard.Listener(on_press=on_press)
+    lis.start() # start to listen on a separate thread
+
+    #this section is covered in publisher_and_subscriber_example.py
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+
+    while True:
+        time.sleep(1)
